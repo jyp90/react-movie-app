73868dfd5194c4654c518586ec97fe5f75e8fdd5
jypark <babungv@gmail.com>
2021-04-19 11:47:53 +0900

    # 2.0 Creating my first React Component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Potato from "./Potato";
 
 function App() {
   return (
@@ -5,6 +6,7 @@ function App() {
       <h1>
         Hello Nyong!! 2021 !!
       </h1>
+        <Potato />
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Potato(){
+    return (
+        <h5>I love potato</h5>
+    )
+}
+
+export default Potato;
