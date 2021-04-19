07fb25fb3d606f4d26bdf1829b679809efc4a6fd
jypark <babungv@gmail.com>
2021-04-19 12:25:45 +0900

    # 2.2 Dynamic Component Generation

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 function Food(props) {
-    return <h3>I like {props.fav}</h3>
+    return <div>
+        <h2>I like {props.name}</h2>
+        <img src={props.picture} />
+    </div>
 }
+
+const foodILike = [{
+    name: "Kimchi",
+    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98.png"
+},{
+    name: "Ramen",
+    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg"
+},{
+    name: "Samgiopsal",
+    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg"
+},{
+    name: "Chukumi",
+    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdh9Dqr%2FbtqzsWLoYqY%2FKgEhm09QyxfW29QfeWlkWk%2Fimg.png"
+}]
 
 function App() {
   return (
@@ -8,10 +25,7 @@ function App() {
       <h1>
         Hello Nyong!! 2021 !!
       </h1>
-        <Food fav="kimchi"/>
-        <Food fav="ramen"/>
-        <Food fav="samgiopsal"/>
-        <Food fav="chukumi"/>
+        {foodILike.map(dish => <Food name={dish.name} picture={dish.image}/>)}
     </div>
   );
 }
