5c08398cda3cb614757f92240a8892119cbacdf2
jypark <babungv@gmail.com>
2021-04-19 15:22:56 +0900

    # 2.4.2 Add ESLint & Prettier Plugin on Project

--- README.md
+++ README.md
@@ -1,3 +1,115 @@
 # Nyong Test App 2021
 
-Create new app for Web Study 
+Create new app for Web Study
+
+## Create Application Command Line
+
+$ npx create-react-app nyong_sample_2021
+
+## Install React Prop Types
+
+$ npm i prop-types
+
+## For WebStorm User
+
+### Install ESLint
+
+1. npm i eslint --save-dev
+2. eslint -g ./node_modules/.bin/eslint --init
+
+- Questions
+
+```
+1. How would you like to use ESLint?
+To check syntax and find problems (style)
+2. Waht type of modules does your project use?
+JavaScript modules (esm)
+3. Which framework does your project use?
+React (react)
+4. Does your porject use TypeScript?
+Yes
+4. Where does your code run?
+Browser (browser)
+5. How would you like to define a style for your project?
+Use a popular style guide (guide)
+6. Which style guide do you want to follow?
+Airbnb (airbnb)
+7. What format do you want your config file to be in?
+JSON
+8. Would you like to install them now with npm?
+Yes
+```
+
+Result: .eslintrc.json
+
+```
+"rules" : {
+  + "no-console" : "off"
+}
+```
+
+## Babel, ESlint Setting
+
+$ npm install --save-dev babel-eslint
+
+```
+{
+ + "parser" : "babel-eslint"
+   "rules" : {
+     ...
+   }
+}
+```
+
+https://www.jetbrains.com/help/webstorm/eslint.html
+
+## Install Prettier
+
+https://prettier.io/docs/en/install.html
+https://prettier.io/docs/en/configuration.html
+
+### $ npm i --save-dev --save-exact prettier
+
+Shortcut : Cmd + Opt + Shift + P
+
+![img.png](img_md/add_new_watcher.png)
+
+- Program : $ProjectFileDir$/node_modules/.bin/prettier
+- Arguments : --write $FilePathRelativeToProjectRoot$
+- Output paths to refresh : $FilePathRelativeToProjectRoot$
+
+### Check Preference
+
+## Setting Prettier 
+
+### .prettierrc.json
+
+```
+{
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "semi": false,
+  "singleQuote": true
+}
+
+
+```
+
+![img.png](img_md/check_prettier_settings.png)
+
+## ESLint, Prettier Settings (두개를 혼용해서 사용하는 경우)
+
+$ npm i --save-dev eslint-config-prettier
+
+### .eslintrc.json
+
+```
+{
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb",
+        "prettier"
+    ],
+}
+
+```
--- src/App.js
+++ src/App.js
@@ -1,43 +1,50 @@
 function Food(props) {
-    return <div>
-        <h2>I like {props.name}</h2>
-        <img src={props.picture} alt={props.name}/>
+  return (
+    <div>
+      <h2>I like {props.name}</h2>
+      <img src={props.picture} alt={props.name} />
     </div>
+  )
 }
 
-const foodILike = [{
+const foodILike = [
+  {
     id: 1,
-    name: "Kimchi",
-    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98.png",
-    rating: 5
-},{
+    name: 'Kimchi',
+    image:
+      'https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98.png',
+    rating: 5,
+  },
+  {
     id: 2,
-    name: "Ramen",
-    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg",
-    rating: 4.9
-},{
+    name: 'Ramen',
+    image:
+      'https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg',
+    rating: 4.9,
+  },
+  {
     id: 3,
-    name: "Samgiopsal",
-    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg",
-    rating: 5.5
-},{
+    name: 'Samgiopsal',
+    image:
+      'https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg',
+    rating: 5.5,
+  },
+  {
     id: 4,
-    name: "Chukumi",
-    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdh9Dqr%2FbtqzsWLoYqY%2FKgEhm09QyxfW29QfeWlkWk%2Fimg.png",
-    rating: 4.7
-}]
+    name: 'Chukumi',
+    image:
+      'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdh9Dqr%2FbtqzsWLoYqY%2FKgEhm09QyxfW29QfeWlkWk%2Fimg.png',
+    rating: 4.7,
+  },
+]
 
-const renderFood = function(dish) {
-    console.log(dish);
-    return <Food key={dish.id} name={dish.name} picture={dish.image} />;
+const renderFood = function (dish) {
+  console.log(dish)
+  return <Food key={dish.id} name={dish.name} picture={dish.image} />
 }
 
 function App() {
-  return (
-    <div>
-        {foodILike.map(renderFood)}
-    </div>
-  );
+  return <div>{foodILike.map(renderFood)}</div>
 }
 
-export default App;
+export default App
