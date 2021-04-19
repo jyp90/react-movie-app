fc4ce2966325c13c1688015109e2e5bfe9776f64
jypark <babungv@gmail.com>
2021-04-19 14:26:08 +0900

    # 2.4.1 Install Prop-types

--- src/App.js
+++ src/App.js
@@ -8,19 +8,23 @@ function Food(props) {
 const foodILike = [{
     id: 1,
     name: "Kimchi",
-    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98.png"
+    image: "https://www.bgw.kr/wp-content/uploads/2019/12/%ED%8F%AC%EA%B8%B0%EA%B9%80%EC%B9%98.png",
+    rating: 5
 },{
     id: 2,
     name: "Ramen",
-    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg"
+    image: "https://health.chosun.com/site/data/img_dir/2020/09/07/2020090702900_0.jpg",
+    rating: 4.9
 },{
     id: 3,
     name: "Samgiopsal",
-    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg"
+    image: "https://newsimg.hankookilbo.com/cms/articlerelease/2020/06/11/202006111362061920_7.jpg",
+    rating: 5.5
 },{
     id: 4,
     name: "Chukumi",
-    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdh9Dqr%2FbtqzsWLoYqY%2FKgEhm09QyxfW29QfeWlkWk%2Fimg.png"
+    image: "https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2Fdh9Dqr%2FbtqzsWLoYqY%2FKgEhm09QyxfW29QfeWlkWk%2Fimg.png",
+    rating: 4.7
 }]
 
 const renderFood = function(dish) {
